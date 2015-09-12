42c22e72e3572ededef747f727ba0b78e54317ac
Everton <evertonagilar@gmail.com>
2015-09-11 23:01:39 -0300

    Pequenos ajustes

--- sae/scripts.js
+++ sae/scripts.js
@@ -2,24 +2,71 @@
 
 var estudoSocioEconomicoForm = {
 
-    onchange : function(field, operacao) {
-    },
-
-    
-    onready : function(field, operacao) {
-    },
-    
     avanca_etapa : function(){
+    	var doc = document;
+    	var f_estudo = doc.getElementById("f_estudo");
+    	var dat_estudo = f_estudo.dataset;
 
-	    if (document.getElementById('f_aceite').checked){
-	        document.getElementById('f_estudo_preliminar').style.display = '';
-	        document.getElementById('f_ler_edital').style.display = 'none';
-	    }else{
-	    	document.getElementById('f_estudo_preliminar').style.display = 'none';
-	    	document.getElementById('f_ler_edital').style.display = '';
-	    	alert("Você deve ler o edital primeiramente!");
-	    }
+    	switch (dat_estudo.passoAtual) {
+    		case "ler_edital":
+    		    if (doc.getElementById('f_aceite').checked){
+    		        doc.getElementById('f_estudo_preliminar').style.display = '';
+    		        doc.getElementById('f_ler_edital').style.display = 'none';
+    		        doc.getElementById("f_estudo").dataset.passoAtual = "estudo_preliminar";
+    		    }else{
+    		    	alert("Você deve ler o edital primeiro!");
+    		    }
+    		    break;
+    		case ("estudo_preliminar"):
+    			doc.getElementById('f_dados_pessoais').style.display = '';
+    			doc.getElementById('f_estudo_preliminar').style.display = 'none';
+    			doc.getElementById("f_estudo").dataset.passoAtual = "dados_pessoais";
+    			break;
+    		case ("dados_pessoais"):
+    			doc.getElementById('f_dados_familiares').style.display = '';
+    			doc.getElementById('f_dados_pessoais').style.display = 'none';
+    			doc.getElementById("f_estudo").dataset.passoAtual = "dados_familiares";
+    			break;
+    		case ("dados_familiares"):
+    			doc.getElementById('f_bens').style.display = '';
+    			doc.getElementById('f_dados_familiares').style.display = 'none';
+    			doc.getElementById("f_estudo").dataset.passoAtual = "bens";
+    			break;
+    	}
+    },
+    
+    volta_etapa : function(){
+    	var doc = document;
+    	var f_estudo = doc.getElementById("f_estudo");
+    	var dat_estudo = f_estudo.dataset;
 
+    	switch (dat_estudo.passoAtual) {
+    		case "ler_edital":
+    			 window.history.back();
+    		    break;
+    		case ("estudo_preliminar"):
+    			doc.getElementById('f_ler_edital').style.display = '';
+    			doc.getElementById('f_estudo_preliminar').style.display = 'none';
+    			doc.getElementById("f_estudo").dataset.passoAtual = "ler_edital";
+    			break;
+    		case ("dados_pessoais"):
+    			doc.getElementById('f_estudo_preliminar').style.display = '';
+    			doc.getElementById('f_dados_pessoais').style.display = 'none';
+    			doc.getElementById("f_estudo").dataset.passoAtual = "estudo_preliminar";
+    			break;
+    		case ("dados_familiares"):
+    			doc.getElementById('f_dados_pessoais').style.display = '';
+    			doc.getElementById('f_dados_familiares').style.display = 'none';
+    			doc.getElementById("f_estudo").dataset.passoAtual = "dados_pessoais";
+    			break;
+    		case ("bens"):
+    			doc.getElementById('f_dados_familiares').style.display = '';
+    			doc.getElementById('f_bens').style.display = 'none';
+    			doc.getElementById("f_estudo").dataset.passoAtual = "dados_familiares";
+    			break;
+
+    	}
     }
+    
 };    
 
